61386845f05d1b7da69e4526c603d87996f126d1
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-02 18:45:04 -0700

    Create test_rotation.py

---
+++ Anti-Bot Scraping Implementation /anti-bot-scraper/test_rotation.py
@@ -0,0 +1,13 @@
+from scraper import AntiDetectionScraper
+
+def test_user_agents():
+    """Test that user agents are rotating."""
+    scraper = AntiDetectionScraper()
+    
+    # TODO: Generate 5 different headers
+    # Print the User-Agent from each
+    # Verify they are different
+    pass
+
+if __name__ == "__main__":
+    test_user_agents()
